fcbf85856c4baf7ceae6a31d05795ec8ffa5cf59
Nockm <nockmss@gmail.com>
2024-04-23 12:49:00 +0100

    refactor: Use theme, move style to App.axaml

---
+++ src/SubStyle.Views/Archive/UserControl1.axaml.cs
@@ -0,0 +1,11 @@
+namespace SubStyle.Views.Archive;
+
+using Avalonia.Controls;
+
+public partial class UserControl1 : UserControl
+{
+    public UserControl1()
+    {
+        this.InitializeComponent();
+    }
+}
